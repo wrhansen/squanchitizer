e56866b95c32c9b8a2105e20cb51976a4344e9ff
Wesley Hansen <wesrhansen@gmail.com>
2019-01-14 12:06:05 -0500

    Squanch App is active

--- squanchitizer/settings.py
+++ squanchitizer/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '2ni%cu4mv2!jaec(4$=*$htp#nl2dbxk22=pm9)-642&t72(o#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'https://wes-squanchitizer.herokuapp.com/']
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'squanch',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
